15327335b5c18044cdcbada8e91cc75e65258ac3
Ben Szekeres <as220@ic.ac.uk>
2021-02-12 17:38:34 +0000

    Installed axios packege, made sure that the url_checker app is implemented as per the Medium tutorial, BUT did not finish the Medium tutorial given some final steps invovled React. Therefore, specifically because of what I added to the bottom of settings.py the runserver command won't work until the React stuff is done as per the tutorial

--- django_react/settings.py
+++ django_react/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework'
+    'url_checker'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Following added by Ben:
+
+REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend') 
+STATICFILES_DIRS = [
+    os.path.join(REACT_APP_DIR, 'build', 'static'),
+]
--- url_checker/views.py
+++ url_checker/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import urllib.request
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+@csrf_exempt
+def check_url(request):
+    try:
+        url_status = urllib.request.urlopen(request.body.decode("utf-8")).getcode()
+    except:
+        return HttpResponse(":(URL is Not Working")
+    if (url_status == 200):
+        return HttpResponse("Yay! URL is Working")
+    return HttpResponse("Yay! URL is Not Working")
