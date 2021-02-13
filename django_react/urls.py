c65c2c57c149d9040510f1aaf8b8971168085736
Ben Szekeres <as220@ic.ac.uk>
2021-02-13 16:03:09 +0000

    Pushing again

--- django_react/urls.py
+++ django_react/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-# from url_checker.views import check_url
+from url_checker.views import check_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
