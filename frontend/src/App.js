f0683a4a077c4e9904f9b21f91e2492688dad6b3
Samuel Valdes <sv1220@ic.ac.uk>
2021-02-13 17:25:22 +0000

    Updating code in App.js and Adding info os proxy server in package.jason files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,40 @@
+import React, { Component } from "react";
+import axios from "axios";
 import './App.css';
-
-function App() {
-  return (
-    <div>
-      <h1>HOLA HOMBRE</h1>
-    </div>
-  );
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      url: '',
+    }
+this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+handleChange(event) {
+    this.setState({url: event.target.value})
+  }
+  handleSubmit(event) {
+    axios
+      .post("/url_checker", this.state.url)
+      .then(res => {
+        alert(res.data)
+      })
+    event.preventDefault();
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+                url:
+                <input type="text" name="url" value={this.state.url} onChange={this.handleChange} />
+            </label>
+              <input type="submit" value="Check URL" />
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
